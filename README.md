d9300dfb9e1e6a93e1447bd4e0bd284eae7bce0f
faqihardi <faqihbackup2024@gmail.com>
2026-09-15 21:28:20 +0700

    submit pertemuan 2

---
+++ README.md
@@ -0,0 +1,14 @@
+﻿# Praktikum Pemrograman Mobile
+
+## Identitas
+- Nama  : Faqih Ardiansyah
+- NIM : H1D024047
+- Shift KRS : A
+- Shift Baru : F
+
+## Screenshot
+| Halaman     | Screenshot                               |
+|-------------|------------------------------------------|
+| Pertemuan 1 | ![Image 1](sreenshots/pertemuan_1)       |
+| Pertemuan 2 | ![Pertemuan 2](sreenshots/pertemuan_2_1) |
+|             | ![Pertemuan 2](sreenshots/pertemuan_2_2) |    
---
+++ app/src/main/java/com/example/faqih/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.faqih
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.faqih.ui.screen.HubungiKamiScreen
+import com.example.faqih.ui.theme.JualanTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.faqih.ui.screen.BasicInfoScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JualanTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "basic_info"
+                    ) {
+                        composable(route = "basic_info") {
+                            BasicInfoScreen(
+                                onNavigateToContact = {
+                                    navController.navigate(route = "form_screen")
+                                }
+                            )
+                        }
+                        composable(route = "form_screen") {
+                            HubungiKamiScreen(navController = navController)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JualanTheme {
+        Greeting("Android")
+    }
+}
